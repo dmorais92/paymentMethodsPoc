71cf204f1d5e7a61690224fde2e3612b24ea88fb
David Morais <davidmorais92@gmail.com>
2019-10-05 17:03:44 +0100

    Replace CSS with styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
 
-function App() {
+function App({ className }) {
   return (
-    <div className="App">
+    <div className={`App ${className}`}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
