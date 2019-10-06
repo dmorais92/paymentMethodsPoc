a4c10e552b6902051304466100bb01c57cb1e856
David Morais <davidmorais92@gmail.com>
2019-10-06 19:55:47 +0100

    Add NODE_ENV variables   - Don't use logger middleware unless we're in 'development'

--- src/Store/index.js
+++ src/Store/index.js
@@ -5,11 +5,17 @@ import throttle from 'lodash.throttle';
 import RootReducer from './root.reducer';
 import { loadState, saveState } from './localStorage';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+const middlewares = [thunk];
+if (isDevelopment) {
+	middlewares.push(logger);
+}
+
 const persistedState = loadState();
 const store = createStore(
 	RootReducer,
 	persistedState,
-	applyMiddleware(logger, thunk)
+	applyMiddleware(...middlewares)
 );
 
 store.subscribe(
