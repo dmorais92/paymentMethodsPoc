aec98b4114650e30eaa36ecbaa633c5a04c8f34b
David Morais <davidmorais92@gmail.com>
2019-10-06 19:31:10 +0100

    Un-eject application   - Eject is redundant since we're no longer using optional chaining syntax   - Move Store container to App component to ease tests

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React from 'react';
 import PaymentMethods from './Components/PaymentMethods';
 import StyledComponents from './Components/Components.styled';
 import { ReactComponent as Logo } from './Components/Icons/logo.svg';
+import Store from './Store';
+import { Provider } from 'react-redux';
 
 const { AppBar } = StyledComponents;
 
 function App({ className }) {
 	return (
-		<div className={`App ${className}`}>
-			<AppBar>
-				<span>Payments</span>
-				<div>
-					<span>
-						Made with React <Logo />
-					</span>
-					<span>by</span>
-					<span>
-						<a href="https://github.com/dmorais92/"> David Morais</a>
-					</span>
-				</div>
-			</AppBar>
-			<PaymentMethods />
-		</div>
+		<Provider store={Store}>
+			<div className={`App ${className}`}>
+				<AppBar>
+					<span>Payments</span>
+					<div>
+						<span>
+							Made with React <Logo />
+						</span>
+						<span>by</span>
+						<span>
+							<a href="https://github.com/dmorais92/"> David Morais</a>
+						</span>
+					</div>
+				</AppBar>
+				<PaymentMethods />
+			</div>
+		</Provider>
 	);
 }
 
